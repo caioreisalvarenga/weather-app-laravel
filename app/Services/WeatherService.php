0aba6fd360adb3aeb315e5b23d54611aa4fe7d66
Caio Alvarenga <caioreisalvarenga@gmail.com>
2024-08-22 14:10:59 -0300

    improving mehtod getWeather service

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -1,26 +1,43 @@
 <?php
 
-   namespace App\Services;
+namespace App\Services;
 
-   use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Http;
 
-   class WeatherService
-   {
-       protected $apiKey;
+class WeatherService
+{
+    protected $apiKey;
 
-       public function __construct()
-       {
-           $this->apiKey = env('OPENWEATHER_API_KEY');
-       }
+    public function __construct()
+    {
+        $this->apiKey = env('OPENWEATHER_API_KEY');
+    }
 
-       public function getWeather($city)
-       {
-           $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
-               'q' => $city,
-               'appid' => $this->apiKey,
-               'units' => 'metric' // Você pode mudar para 'imperial' se preferir
-           ]);
+    public function getWeather($city)
+    {
+        try {
+            $response = Http::withOptions(['verify' => false])->get("https://api.openweathermap.org/data/2.5/weather", [
+                'q' => $city,
+                'appid' => $this->apiKey,
+                'units' => 'metric'
+            ]);
 
-           return $response->json();
-       }
-   }
+            $weatherData = $response->json();
+
+            if ($response->failed()) {
+                return [
+                    'error' => $weatherData['message'] ?? 'Erro ao obter dados meteorológicos.',
+                    'code' => $weatherData['cod'] ?? 500
+                ];
+            }
+
+            return $weatherData;
+
+        } catch (\Exception $e) {
+            return [
+                'error' => 'Ocorreu um erro ao obter os dados meteorológicos.',
+                'code' => 500
+            ];
+        }
+    }
+}
